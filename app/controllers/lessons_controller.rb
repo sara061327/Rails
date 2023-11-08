b017f4d5ace8276c3544c1aafd6270552b359fca
月舘沙良 <tsukidatesara@tsukidatesaranoMacBook-Air.local>
2023-11-09 01:14:13 +0900

    commit

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,9 @@
+class LessonsController < ApplicationController
+  def hello
+      @hello = "こんにちは"
+      @message = "helloアクションが実行されています"
+  end
+  def call
+      @call = "callアクションが実行されています"
+  end
+end
